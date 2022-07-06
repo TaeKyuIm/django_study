5bdff5eaaf86311ea2b58e4a2c8cba9712f1a408
임태규 <kyu5787@naver.com>
2022-07-06 18:42:57 +0900

    drf todo

--- drf_todo/todo/views.py
+++ drf_todo/todo/views.py
@@ -31,7 +31,7 @@ class TodoAPIView(APIView):
         serializer = TodoCreateSerializer(todo, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class DoneTodosAPIView(APIView):
